3cf03566540fe09428f8f36d788708da84485d17
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 09:05:02 +0300

    Problem 2

---
+++ EulerProblem2/EulerProblem2/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let rec fibonacciRec a b =
+  if a + b < 4000000 then
+    let current = a + b
+    let rest = fibonacciRec b current  
+    current :: rest
+  else 
+    []
+
+let isEven x = x % 2 = 0
+   
+[<EntryPoint>]
+let main argv = 
+    let fibonacciList = 1::2::(fibonacciRec 1 2)
+    Console.WriteLine(fibonacciList |> List.filter isEven |> List.sum)
+    Console.ReadLine() |> ignore
+    0
