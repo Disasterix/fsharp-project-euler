5a536c3bef587a07021da2b12549a0be2492cac4
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 08:25:52 +0300

    Initial commit

---
+++ EulerProblem1/EulerProblem1/Program.fs
@@ -0,0 +1,21 @@
+﻿open System.Collections.Generic
+open System
+
+let list = new List<int>()
+let mutable total = 0;
+let checkNumber x = (x%3 = 0)||(x%5 = 0)
+
+let count() = 
+    for i = 1 to 999 do
+        if checkNumber(i) then
+            list.Add(i)
+        
+[<EntryPoint>]
+let main argv = 
+    count()
+    for number in list do
+        total <- total + number
+    Console.WriteLine(total)
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
+
