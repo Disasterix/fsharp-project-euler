e80633ef5a007c451c8a5125ec939f158c53b862
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 10:27:18 +0300

    Problem 3

---
+++ EulerProblem3/EulerProblem3/Program.fs
@@ -0,0 +1,16 @@
+﻿let largestPrimeFactor n =
+    let rec factorise n x y = 
+        if x = n then
+            x::y
+        elif n % x = 0L then
+            factorise (n/x) x (x::y)
+        else
+            factorise n (x+1L) y
+    factorise n 2L [] |> List.max
+let fac = largestPrimeFactor 600851475143L 
+
+[<EntryPoint>]
+let main argv = 
+    fac |> printfn "%i" 
+    System.Console.ReadKey(true) |> ignore 
+    0
