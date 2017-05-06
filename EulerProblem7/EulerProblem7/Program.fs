7fc70339a0e1176f0a5ac13452e27e13bd7eaab7
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 16:06:25 +0300

    Problem 7

---
+++ EulerProblem7/EulerProblem7/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let prime n =
+    Seq.exists (fun x -> (n % x = 0))
+
+let isPrime n =
+    match n with
+        | _ -> seq { 2 .. Convert.ToInt32(Math.Sqrt(float n))}
+        |> prime n
+        |> not
+
+let primes =
+    Seq.initInfinite (fun i -> i + 2)
+    |> Seq.filter isPrime
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" (primes |> Seq.take 10001 |> Seq.max )
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
