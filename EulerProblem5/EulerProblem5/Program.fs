26e22c725c63b1aba3f9bf19089a79c1e4983519
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 13:45:16 +0300

    Problem 5

---
+++ EulerProblem5/EulerProblem5/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let rec searchDivisible n a =
+    let rec isDiv n a =
+        a |> List.filter (fun x -> n % x = 0)
+    let result = isDiv n a
+
+    if result = a then
+        n
+    else
+        searchDivisible (n+1) a
+    
+[<EntryPoint>]
+let main argv = 
+    searchDivisible 1 [1..20] |> printfn "%i" 
+    Console.ReadKey() |> ignore
+    0
