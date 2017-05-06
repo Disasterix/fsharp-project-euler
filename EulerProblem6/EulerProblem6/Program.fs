8ffac81cec8e5c9701f88c710c36981bdf8be9b1
Roope Thomsson <roope.thomsson@gmail.com>
2017-05-06 14:29:29 +0300

    Problem 6

---
+++ EulerProblem6/EulerProblem6/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    let square x = x*x
+    Console.WriteLine (square ([1..100] |> List.sum) - ([1..100] |> List.map square |> List.sum ))
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
